01de481047e2e5b9d3dee681bb91bda14f5244cc
SSDietrichSS <kdj287zt@hotmail.com>
2016-12-28 14:30:19 +0100

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Update()
+    {// sterowanie góra dół
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddTorque(-Vector3.left * 25);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddTorque(Vector3.left * 25);
+        }
+        //sterowanie prawo lewo
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddTorque(-Vector3.forward * 25);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddTorque(Vector3.forward * 25);
+        }
+
+    }
+}
---
+++ NewBehaviourScript1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    //camera
+    public Transform sphere;
+    void Update()
+    {
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+        Vector3 vector = new Vector3(0, 5f, -5f);
+        float velocity = rigidbody.velocity.sqrMagnitude;
+        Debug.Log(velocity);
+        Debug.Log(rigidbody.velocity.sqrMagnitude);
+        vector =vector*(1f+velocity);
+        transform.position = sphere.position + (vector);
+        transform.LookAt(sphere);
+
+    }
+}
